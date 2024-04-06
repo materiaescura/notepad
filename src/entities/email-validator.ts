053c01e6246fb94a53c863d72732cf8accb8071b
Diego Rabelo <rabelo.github@gmail.com>
2024-04-06 08:47:53 -0300

    feat: validate email for strings with less than 321 chars

--- src/entities/email-validator.ts
+++ src/entities/email-validator.ts
@@ -1,10 +1,14 @@
 export function valid(email: string): boolean {
   const maxEmailSize = 320
-  const validators = [isEmpty]
+  const validators = [isEmpty, isTooLarge.bind(null, maxEmailSize)]
 
   return validators.every((validator) => validator(email) === false)
 }
 
 function isEmpty(str: string): boolean {
   return !str ? true : false
+}
+
+function isTooLarge(maxSize: number, str: string): boolean {
+  return str.length > maxSize ? true : false
 }
