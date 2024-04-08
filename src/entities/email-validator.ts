ab627bcc02ad61326072a7a0f75224f63d5aec14
Diego Rabelo <rabelo.github@gmail.com>
2024-04-08 08:20:01 -0300

    feat: validate domain email for strings up to 255 chars

--- src/entities/email-validator.ts
+++ src/entities/email-validator.ts
@@ -1,6 +1,10 @@
 export function valid(email: string): boolean {
   const maxEmailSize = 320
-  const validators = [isEmpty, isTooLarge.bind(null, maxEmailSize)]
+  const validators = [
+    isEmpty,
+    isTooLarge.bind(null, maxEmailSize),
+    domainTooLarge,
+  ]
 
   return validators.every((validator) => validator(email) === false)
 }
@@ -11,4 +15,9 @@ function isEmpty(str: string): boolean {
 
 function isTooLarge(maxSize: number, str: string): boolean {
   return str.length > maxSize ? true : false
+}
+
+function domainTooLarge(email: string): boolean {
+  const [_, domain] = email.split('@')
+  return isTooLarge(255, domain)
 }
